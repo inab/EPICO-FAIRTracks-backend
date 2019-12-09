c11988f06b3e0dbd08f3c92de05fd90eee4963a9
José María Fernández <jose.m.fernandez@bsc.es>
2019-12-09 01:07:24 +0100

    v0.0.3

--- lib/EPICO/REST/Backend/FAIRTracks.pm
+++ lib/EPICO/REST/Backend/FAIRTracks.pm
@@ -12,7 +12,7 @@ package EPICO::REST::Backend::FAIRTracks;
 
 use version;
 
-our $VERSION = version->declare('v0.0.2');
+our $VERSION = version->declare('v0.0.3');
 
 use base qw(EPICO::REST::Backend);
 
